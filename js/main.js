fb1848b330ad1188793ab3ec780afa27130b4d03
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2025-03-09 23:23:17 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -342,16 +342,8 @@ class Header
         this.#loaded = true;
         
         Data.html.main.style.minHeight = `${window.innerHeight - 93}px`;
-        Data.html.main.style.height = Data.html.main.clientHeight - window.innerHeight < 0 ? `${window.innerHeight - 93}px` : "";
 
-        window.addEventListener("resize", async () => {
-            Data.html.main.style.height = "";
-
-            await new Promise(resolve => requestAnimationFrame(resolve));
-
-            Data.html.main.style.minHeight = `${window.innerHeight - 93}px`;
-            Data.html.main.style.height = Data.html.main.clientHeight - window.innerHeight < 0 ? `${window.innerHeight - 93}px` : "";
-        });
+        window.addEventListener("resize", () => Data.html.main.style.minHeight = `${window.innerHeight - 93}px`);
         
         Loop.Append(() => this.Update());
     }
